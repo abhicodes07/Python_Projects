7d9ed8bf42e37a746a3a4f0bc902536d05d22143
Abhinav <abhiasthana76@gmail.com>
2024-06-04 14:39:24 +0530

    Program upon whether entered number is prime or not

---
+++ prime.py
@@ -0,0 +1,17 @@
+
+# This is a program about finding whether a number is prime or not
+num = int(input("Enter the number: "))
+
+flag = 0
+if num == 0 or num == 1:
+    flag = 1
+for i in range(2, num):
+    if num % i == 0:
+        flag = 1
+        break
+if flag == 0:
+    print("Entered number is a prime number.")
+else:
+    print("Entered number is not a prime number.")
+
+
