a65a6aa86656684995cb8b61ad4824103ad0bf26
Abhinav <abhiasthana76@gmail.com>
2024-06-05 21:24:39 +0530

    Program to make a right angled star triangle pattern.

---
+++ star_pattern.py
@@ -0,0 +1,17 @@
+
+from time import sleep
+
+# This is program about printing a star pattern using a for loop
+
+# First we need to ask the user how many rows needs to be printed
+rows = int(input("Enter the number: "))
+
+for i in range(rows):
+    print()
+    sleep(1)
+    for j in range(i+1):
+        print('*', end=' ')
+        
+
+print()
+
