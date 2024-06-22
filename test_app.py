adaf75ac8c85d93578c102bcf801c325f1defe09
Abhinav <abhiasthana76@gmail.com>
2024-06-22 15:51:38 +0530

    testing

--- test_app.py
+++ test_app.py
@@ -1,2 +1,17 @@
-# user = input("Enter the username: ")
-# pass = input("Enter the usernamw: ")
+user = input("Enter the username: ").lower
+passw = input("Enter the username: ").lower
+
+
+print(user)
+print(passw)
+
+username = input("Enter the username again: ").lower
+password = input("Enter the password again: ").lower
+print(username)
+print(password)
+
+while username != user and password != passw:
+    username = input("Enter the correrct username: ")
+    password = input("Enter the correct password: ")
+
+
